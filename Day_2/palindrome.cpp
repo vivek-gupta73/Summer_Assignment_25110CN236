ce7eccb57267d0eff3b717fa424c7cdfa4d6f428
vivek-gupta73 <vivek.831gupta@gmail.com>
2026-05-29 17:32:39 +0530

    Day 2 4th code

---
+++ Day_2/palindrome.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num, original, reverse = 0, digit;
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    original = num;
+
+    while (num != 0) {
+        digit = num % 10;                    
+        reverse = reverse * 10 + digit;     
+        num = num / 10;                   
+    }
+
+    if (original == reverse)
+        cout << "Palindrome number";
+    else
+        cout << "Not a palindrome number";
+
+    return 0;
+}
