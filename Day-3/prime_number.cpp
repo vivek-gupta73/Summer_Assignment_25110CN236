88c5076cf0be9786818cfd0db3e6e3ebd0ca20a5
vivek-gupta73 <vivek.831gupta@gmail.com>
2026-05-30 22:16:05 +0530

    Day 3 1st code

---
+++ Day-3/prime_number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i, count = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for(i = 1; i <= n; i++)
+    {
+        if(n % i == 0)
+        {
+            count++;
+        }
+    }
+
+    if(count == 2)
+    {
+        cout << "Prime Number";
+    }
+    else
+    {
+        cout << "Not a Prime Number";
+    }
+
+    return 0;
+}
