4046c774a88cef6cda407a45221a4ab800a58a96
vivek-gupta73 <vivek.831gupta@gmail.com>
2026-05-30 22:24:58 +0530

    Day 3 4th code

---
+++ Day-3/LCM_of_two_numbers.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, max;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    max = (a > b) ? a : b;
+
+    while(true)
+    {
+        if(max % a == 0 && max % b == 0)
+        {
+            cout << "LCM = " << max;
+            break;
+        }
+        max++;
+    }
+
+    return 0;
+}
