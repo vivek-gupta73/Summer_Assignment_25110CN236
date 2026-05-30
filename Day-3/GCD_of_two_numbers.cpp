0a0c5736eecbdb7ffb50f80ce07fd8a91e131112
vivek-gupta73 <vivek.831gupta@gmail.com>
2026-05-30 22:22:34 +0530

    Day 3 3rd code

---
+++ Day-3/GCD_of_two_numbers.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, i, gcd;
+
+    cout << "Enter two numbers: ";
+    cin >> a >> b;
+
+    for(i = 1; i <= a && i <= b; i++)
+    {
+        if(a % i == 0 && b % i == 0)
+        {
+            gcd = i;
+        }
+    }
+
+    cout << "GCD = " << gcd;
+
+    return 0;
+}
