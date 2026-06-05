805b5d8de1e0c07022154e042dab6b3358067c44
vivek-gupta73 <vivek.831gupta@gmail.com>
2026-06-05 15:55:30 +0530

    Day-4 uploaded.

---
+++ Day-4/Fibonacci_series.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, a = 0, b = 1, c;
+
+    cout << "Enter number of terms: ";
+    cin >> n;
+
+    cout << "Fibonacci Series: ";
+
+    for(int i = 1; i <= n; i++)
+    {
+        cout << a << " ";
+
+        c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+} 
+
+   // Start from 0 and 1.
+
+   // Add the previous two numbers to get the next number in the series.
